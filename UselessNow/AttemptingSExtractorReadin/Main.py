b9cdae30876152ac68f1b8cdd76cd8692d26e5da
diabolique- <gillen314@gmail.com>
2015-01-31 13:24:29 -0600

    deleted old files
    I don’t know why I decided to keep old versions that didn’t work, but I
    deleted them now.

--- UselessNow/AttemptingSExtractorReadin/Main.py
+++
@@ -1,8 +0,0 @@
-#import image class
-import Image
-
-# Ask for f name
-# filename = raw_input("What is the name of the SExtractor catalog you want to work with? ")
-#for now, just use the test
-filename = "test.sexcat"
-image = Image.Image(filename)
